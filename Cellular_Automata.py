b193725ab8897e37d3e0ba8515fdbc44bac5a5eb
Hani Jandali <hanijandali@Hanis-MacBook-Pro.local>
2020-10-19 01:20:29 -0700

    This commit solves an issue found within the num_cols and num_rows. Needed to place them under the class and redefine as integers

--- Cellular_Automata.py
+++ Cellular_Automata.py
@@ -1,8 +1,9 @@
 import sys
 import pygame
 import pygame.draw
+import random
 
-GridSize = width, height = 620, 440
+GridSize = width, height = 640, 480
 CellSize = 10
 SquareDeadColor = 0, 0, 0
 SquareAliveColor = 0, 180, 200
@@ -16,55 +17,63 @@ class LifeGame:
         pygame.display.flip()
 
         self.init_grids()
-        #Initializes the screen
+            #Initializes the screen
     
     def init_grids(self):
-        #Initializes the grids on which our generations will be displayed
-        num_cols = int(width / CellSize)
-        num_rows = int(height / CellSize)
-        #need to create the grid
-        #This will be a 2D array
-        self.grids = (
-            [[0] * num_rows] * num_cols,
-            [[0] * num_rows] * num_cols
-        )
+            #Initializes the grids on which our generations will be displayed
+        self.num_cols = int(width / CellSize)
+        self.num_rows = int(height / CellSize)
+            #Debugging Information
+        print("Columns: %d\nRows: %d" %(self.num_cols, self.num_rows))
+            #need to create the grid
+            #This will be a 2D array
+        self.grids = [
+            [[0] * self.num_rows] * self.num_cols,
+            [[0] * self.num_rows] * self.num_cols
+        ]
         self.active_grids = 0
-        print(self.grids[0])
-        #above formula was found on Stack Overflow for 2D array of Zeros and modified for 
-        #our variables
-        #The 0 initializes it
-        #The tuple should be immutable. We will see if we can change the list within
 
+        self.randomize_grid()
+        print(self.grids[0])
+            #above formula was found on Stack Overflow for 2D array of Zeros and modified for 
+            #our variables
+            #The 0 initializes it
+            #The tuple should be immutable. We will see if we can change the list within
+
+    def randomize_grid(self): 
+        for c in range(self.num_cols):
+            for r in range(self.num_rows):
+                self.grids[self.active_grids][c][r] = random.choice([0, 1])
 
 
     def draw_grid(self):
-        #circle_rect = pygame.draw.circle(self.screen, SquareAliveColor, (50, 50), 5, 0)
+            #circle_rect = pygame.draw.circle(self.screen, SquareAliveColor, (50, 50), 5, 0)
         pygame.display.flip()
 
 
     def clear_screen(self):
         self.screen.fill(SquareDeadColor)
-        #Gets rid of previous game display
+            #Gets rid of previous game display
 
     def update_generation(self):
-        # Inspect current active generation
-        # update inactive grid to store next generation
-        # swap out the active grid
+            # Inspect current active generation
+            # update inactive grid to store next generation
+            # swap out the active grid
         pass
 
  
     def handle_events(self):
         for event in pygame.event.get():
-            # if event is keypress of "s" then stop the game
-            # if the event is keypress "r" then we will randomize grid
-            # if event is keypress of "1" then we will quit
+                # if event is keypress of "s" then stop the game
+                # if the event is keypress "r" then we will randomize grid
+                # if event is keypress of "1" then we will quit
             if event.type == pygame.QUIT:
                 sys.exit()
 
 
     def run(self):
-        #The main game loop, where everything comes together. This is indicative of the 
-        # multiple passing generations with Cellular Automata
+            #The main game loop, where everything comes together. This is indicative of the 
+            # multiple passing generations with Cellular Automata
         while True:
             self.handle_events()
             self.update_generation() #Might need to add time checking with generations so loop isnt quick
